ca3ad46a60f971ace74ef7ba0890a0377f473d90
rabiul3000 <mdrabiul.asia@gmail.com>
2024-09-10 19:49:08 +0600

    added and fixed some bad code

--- src/componants/navbar/Navbar.jsx
+++ src/componants/navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 			</div>
 			<div className='links'>
 				<div className='github'>
-					<a href='http://www.github.com' target='_blank'>
+					<a href='http://www.github.com/rabiul3000' target='_blank'>
 						<motion.img
 							whileHover={{ scale: 1.2, transition: { duration: 0.2 } }}
 							src='/github.png'
@@ -19,7 +19,7 @@ const Navbar = () => {
 					</a>
 				</div>
 				<div className='facebook'>
-					<a href='http://www.facebook.com' target='_blank'>
+					<a href='https://www.facebook.com/people/Md-Rabiul/61557375413667/?mibextid=ZbWKwL' target='_blank'>
 						<motion.img
 							whileHover={{ scale: 1.2, transition: { duration: 0.2 } }}
 							src='/facebook.png'
@@ -38,6 +38,15 @@ const Navbar = () => {
 						/>
 					</a>
 				</div>
+
+				<motion.div
+					className='dev'
+					whileHover={{ scale: 1.2, transition: { duration: 0.2 } }}
+					onMouseEnter={() => setEnter(true)}
+					onMouseLeave={() => setEnter(false)}
+				>
+					website is still under development
+				</motion.div>
 			</div>
 		</div>
 	);
